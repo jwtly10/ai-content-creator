a252f891e60e6ce5f6b519f615ea3a0b9d91d022
Josh Watley <watleyjosh@gmail.com>
2023-12-17 11:55:55 +0000

    Initial commit

---
+++ src/main/java/com/jwtly10/aicontentgenerator/AiContentGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.jwtly10.aicontentgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiContentGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiContentGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtly10/aicontentgenerator/AiContentGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtly10.aicontentgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiContentGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
