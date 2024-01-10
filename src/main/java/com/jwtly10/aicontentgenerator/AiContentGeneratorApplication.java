eb2fc2d5c1d5b5a2f812f251f6c9d66ee8c4e994
Josh Watley <watleyjosh@gmail.com>
2024-01-10 21:53:08 +0000

    fix: Fixing reddit parser job not reading config

--- src/main/java/com/jwtly10/aicontentgenerator/AiContentGeneratorApplication.java
+++ src/main/java/com/jwtly10/aicontentgenerator/AiContentGeneratorApplication.java
@@ -4,9 +4,11 @@ import com.jwtly10.aicontentgenerator.config.AsyncSchedulingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @Import(AsyncSchedulingConfig.class)
+@EnableRetry
 public class AiContentGeneratorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jwtly10/aicontentgenerator/service/Reddit/RedditPostParserService.java
+++ src/main/java/com/jwtly10/aicontentgenerator/service/Reddit/RedditPostParserService.java
@@ -6,6 +6,7 @@ import com.jwtly10.aicontentgenerator.exceptions.RedditPostParserException;
 import com.jwtly10.aicontentgenerator.model.Reddit.RedditPost;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,7 +22,7 @@ public class RedditPostParserService {
      * @return RedditPost object
      * @throws RedditPostParserException if unable to parse the post
      */
-    @Retryable
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 10000))
     public RedditPost parseRedditPost(String postUrl) throws RedditPostParserException {
         log.info("Parsing reddit post: {}", postUrl);
 
